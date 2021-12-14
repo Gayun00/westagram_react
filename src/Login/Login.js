743a00c8d9c84ba32f6a65c8a9a06828d576e157
gy <gayunlee11@gmail.com>
2021-12-14 17:27:51 +0900

    [Add] Login: 로그인 유효성 검사 추가

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './Login.module.css';
 import '../App.css'
 
 function Login() {
+  const idRef = useRef();
+  const pwRef = useRef();
+  const formRef = useRef();
+  const buttonRef = useRef();
+  const noticeRef = useRef();
+
+  const [validState, setValidState] = useState();
+
+  function handleInput() {
+    const idInputVal = idRef.current.value;
+    const pwInputVal = pwRef.current.value;
+    const regex = new RegExp("@");
+    const validId = regex.exec(idInputVal)
+    if(validId && pwInputVal.length >= 5) {
+      setValidState(true)
+    } else {
+      setValidState(false)
+    }
+  }
+
   return (
 
     <>
-    <div className={styles.container}>
+    <div className={styles.login}>
       <h1 className={styles.logo}>Westagram</h1>
-      <form action="" className={styles.form}>
-        <div className={styles.input_wrapper}>
-          <input type="text" className={styles.id}
+      <form action="" className={styles.form} ref={formRef} onChange={handleInput}>
+        <div className={styles.input}>
+          <input type="text" className={styles.id} ref={idRef}
             placeholder="  전화번호, 사용자 이름 또는 이메일"/>
-          <input type="password" className={styles.password}
+          <input type="password" className={styles.password} ref={pwRef}
               placeholder="  비밀번호"/>
         </div>
-        <button className={styles.login_button}>로그인</button>
-        <p className={styles.notice, styles.hide}>아이디는 @를 포함하고, 비밀번호는 5글자 이상이어야 합니다.</p>
+        <button className={styles.button} ref={buttonRef}>로그인</button>
+        {validState ?'': <p className={styles.notice} ref={noticeRef}>아이디는 @를 포함하고, 비밀번호는 5글자 이상이어야 합니다.</p> }
       </form>
       <div className={styles.forgot_password}>비밀번호를 잊으셨나요?</div>
     </div>
